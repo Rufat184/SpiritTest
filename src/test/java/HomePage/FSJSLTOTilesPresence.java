cde3aa99c60c9df8be64d914a88ff3d3b6972cd4
Rufat Dadashov <rufat.dadashov@spirit.com>
2022-12-11 14:13:49 -0500

    Spirit First Build

--- src/test/java/HomePage/FSJSLTOTilesPresence.java
+++ src/test/java/HomePage/FSJSLTOTilesPresence.java
@@ -12,18 +12,37 @@ import java.util.List;
 
 public class FSJSLTOTilesPresence extends SetUp {
     HomePageElements MyElements;
+
     @Test
     public void LandingOnHome() throws InterruptedException {
         App().Pages().myHomePage().LandingOnHomePage();
     }
+
     @Test(dependsOnMethods = {"LandingOnHome"}, alwaysRun = true)
-    public void checkAvailability(){
+    public void freeSpiritTileIsDisplayed() {
         MyElements = new HomePageElements(driver);
         List<WebElement> myTiles = MyElements.FreeSpiritTiles;
         for (int i = 0; i < myTiles.size(); i++) {
             System.out.println(myTiles.get(i).getText());
             Assert.assertTrue(myTiles.get(0).isDisplayed(), "Free Spirit Tile Should be Displayed");
-            Assert.assertTrue(myTiles.get(1).isDisplayed(), "Join Savers Club Tile Should be Displayed");
+        }
+    }
+
+    @Test(dependsOnMethods = {"LandingOnHome"}, alwaysRun = true)
+    public void joinSpiritSaversClubTileIsDisplayed() {
+        MyElements = new HomePageElements(driver);
+        List<WebElement> myTiles = MyElements.FreeSpiritTiles;
+        for (int i = 0; i < myTiles.size(); i++) {
+            System.out.println(myTiles.get(i).getText());
+            Assert.assertTrue(myTiles.get(2).isDisplayed(), "Limited Time Only Tile Should be Displayed");
+        }
+    }
+    @Test(dependsOnMethods = {"LandingOnHome"}, alwaysRun = true)
+    public void limitedTimeOnlyTileIsDisplayed() {
+        MyElements = new HomePageElements(driver);
+        List<WebElement> myTiles = MyElements.FreeSpiritTiles;
+        for (int i = 0; i < myTiles.size(); i++) {
+            System.out.println(myTiles.get(i).getText());
             Assert.assertTrue(myTiles.get(2).isDisplayed(), "Limited Time Only Tile Should be Displayed");
         }
     }
