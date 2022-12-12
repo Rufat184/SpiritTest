6594d25118640e4328001b8827b37b747d2b67ee
Rufat Dadashov <rufat.dadashov@spirit.com>
2022-12-12 17:45:36 -0500

    Spirit Update

--- src/main/java/Pages/SetUp.java
+++ src/main/java/Pages/SetUp.java
@@ -2,10 +2,12 @@ package Pages;
 
 import Library.AppLib;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Optional;
@@ -14,9 +16,7 @@ import org.testng.annotations.Parameters;
 import java.time.Duration;
 
 public class SetUp {
-
     public WebDriver driver;
-
     private AppLib app;
     @Parameters({"Browser", "driverPathChrome", "driverPathFirefox"})
     @BeforeTest(alwaysRun = true)
@@ -32,14 +32,19 @@ public class SetUp {
             App().Flow().navigateToUrl("https://qa01.nk.spirit.com/");
         } else if (Browser.equals("Firefox")){
             //System.setProperty("webdriver.gecko.driver", exeF);
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.setAcceptInsecureCerts(true);
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-            App().Flow().navigateToUrl("https://qa04.nk.spirit.com/");
+            driver = new FirefoxDriver(firefoxOptions);
+            App().Flow().navigateToUrl("https://qa01.nk.spirit.com/");
         } else if (Browser.equals("Safari")){
             //System.setProperty("webdriver.gecko.driver", exeF);
+            WebDriver driver = new SafariDriver();
+            JavascriptExecutor js = (JavascriptExecutor)driver;
+            js.executeScript("CertificateWarningController.visitInsecureWebsiteWithTemporaryBypass()");
             WebDriverManager.safaridriver().setup();
-            driver = new SafariDriver();
-            App().Flow().navigateToUrl("https://qa04.nk.spirit.com/");
+            //driver = new SafariDriver();
+            App().Flow().navigateToUrl("https://qa01.nk.spirit.com/");
             driver.manage().window().maximize();
         }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
